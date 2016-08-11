25540ed35dd9459644549a1312e40b9fc8b00591
lizhiwei <550822844@qq.com>
2016-08-12 00:17:53 +0800

    start to extract common header

---
+++ learnvue/js/index.js
@@ -0,0 +1,30 @@
+var name ='lzw';
+Vue.component("test",{
+	template:`
+		<div class="test">test ${name} </div>
+	`
+})
+
+
+new Vue({
+	el:"#app",
+	data:{
+		msg:"hello",
+		num:1
+	}
+});
+
+/
+
+/info
+
+/random
+
+/edit
+
+/article/:id
+
+/upload
+
+/play
+
